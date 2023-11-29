df22f5d3c68c8f8d7db868363ef280beabff1dc6
Porkalaikarthika <porkalaikarthika@gmail.com>
2023-11-29 21:22:30 +0530

    fixed the checkbox issue by correcting the function scope

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,7 @@ import Addtodo from './Addtodo'
 
 
 
-// export default function App() {
- 
-//   const [task, setTask] = useState([{Id:1,Title:"",Des:"",done:false
 
-//   }])
-//  var id=1;
-//   function add(title,des)
-//   {
-   
-//     setTask(  [...task,{Id:id++,Title:title,Des:des,done:false}]);
-//   }
-
-  
-
-//   return (
-//     <>
-//     <h1>Task Management App</h1>
-//     <label>
-//     <input type='text'
-//     name='title'
-//      //value={title}
-//     onChange={e=>setTitle(e.target.value)}
-//     />
-//     <input type="text" 
-//     //value={des}
-//     name='description'
-//     onChange={e=>setDes(e.target.value)}/>
-//     <button onClick={()=>
-//       {
-//         add(title,des);
-//         setTitle('');
-//         setDes('');
-//         }
-//         }>Add</button>
-//     </label>
-//     <ul>
-//     {task.map(artist => (
-//       <>
-//           <li key={artist.Id}><b>Title:</b>{artist.Id}{artist.Title}{""}
-//           Description:{artist.Des}</li>
-//           <button onClick={(()=>{
-//             edit(artist.Id,artist.Title,artist.Des)
-//           })}>edit</button>
-//           <button>delete</button>
-//           </>
-//         ))}
-//     </ul>
-    
-//     </>  
-//   );
-// }
 
 
 export default function App(){
@@ -68,6 +18,7 @@ export default function App(){
   setTasklist(pre=>{
     return [...pre,{id:pre.length+1,Title:title,Des:des,done:false}]
   })
+}
  function handleTask(e,id){
       const {name,checked,type,value}=e.target;
       setTasklist((tasks)=>tasks.map((task)=>
@@ -75,7 +26,7 @@ export default function App(){
       )
  }
 
- }
+ 
 
   return (
     <>
@@ -83,7 +34,7 @@ export default function App(){
     <Addtodo handleClick={handleClick}/>
     <ul>
       {tasklist.map((task)=>(<li key={task.id}>
-        <input type="checkbox" name="done"checked={task.done}
+        <input type="checkbox" name="done" checked={task.done}
          onChange={(e)=>handleTask(e,task.id)} />
         Title:{task.Title} Description:{task.Des}
         <button >Edit</button>
